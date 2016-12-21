05b74a5ae1cc5907fa7cc8613fb93a9a8551a7a0
Clemens Kaserer <clemens.kaserer@gmail.com>
2016-12-21 14:26:50 +0100

    travis - check exit codes README - add travis badge

--- .tests/test.sh
+++ .tests/test.sh
@@ -11,11 +11,14 @@ err() {
 }
 
 run_cmd docker run --rm -t tftp-hpa -V
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 
 echo
 echo "===> Start server."
-run_cmd docker run -d -p 69:69/udp -v $(pwd)/.tests/fixtures:/tftpboot/site:ro --name tftpd tftp-hpa
+run_cmd docker run -d -v $(pwd)/.tests/fixtures:/tftpboot/site:ro --name tftpd tftp-hpa
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 ip=$(docker inspect --format '{{.NetworkSettings.IPAddress}}' tftpd | tr -d '\r')
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 [ "x" = "x${ip}" ] && err "Is tftpd container running?"
 echo "Server is up at $ip"
 
@@ -25,21 +28,31 @@ echo
 echo "===> Test that we can download files via tftp client."
 echo "---> /site/menu (fully-qualified, forward slashes)"
 run_cmd tftp $ip -c get /site/menu
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 test -s /tmp/menu
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 grep 'Sample pxe menu' /tmp/menu
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 echo
 echo "---> \site\menu (fully-qualified, backslashes)"
 run_cmd tftp $ip -c get \\site\\menu
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 test -s /tmp/menu
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 grep 'Sample pxe menu' /tmp/menu
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 echo
 echo "---> pxelinux.0 (not fully-qualified)"
 run_cmd tftp $ip -c get pxelinux.0
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 test -s /tmp/pxelinux.0
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 echo
 echo "---> /pxelinux.cfg/default"
 run_cmd tftp $ip -c get /pxelinux.cfg/default
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 test -s /tmp/default
+[ "$?" -eq "0" ] || return $EXIT_FAILURE
 echo
 echo "---> pxelinux.cfg/F1.msg (should be downcased by map file)"
 run_cmd tftp $ip -c get /pxelinux.cfg/F1.msg
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.org/ckaserer/docker-tftp-hpa.svg?branch=master)](https://travis-ci.org/ckaserer/docker-tftp-hpa)
+
 # docker-tftp
